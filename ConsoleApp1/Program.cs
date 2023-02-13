8fa02c7cf3539523beee46b401ff815c44c389e1
akhila.mp <akhila.mp@AKHILAMP>
2023-02-13 15:27:46 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            int temp;
+
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("After Swapping");
+            Console.WriteLine("a : " + a);
+            Console.WriteLine("b : " + b);
+        }
+    }
+}
